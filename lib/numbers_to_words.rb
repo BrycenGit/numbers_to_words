eefca626e589e9cc64a7937b20130f7b93d85a91
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-22 16:05:51 -0700

    Add functionality for hundreds and thousands with passing tests
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -4,46 +4,74 @@ class Number
     @length = @number.to_s.length
     @array = @number.to_s.split("").reverse()    
     @digits = {'0'=>"zero",'1'=>'one','2'=>'two','3'=>'three','4'=>'four','5'=>'five','6'=>'six','7'=>'seven','8'=>'eight','9'=>'nine'}
-    @tens = {'2'=>'twenty', '3'=>'thirty', '4'=>'fourty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety',}
+    @tens = {'2'=>'twenty', '3'=>'thirty', '4'=>'fourty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety'}
   end
 
   def get_word 
-    if (@length == 3)
-      if (@array[1] == '1') 
-        hundreds_result = hundreds() 
-        teens_result = get_teens()
-        return "#{hundreds_result} hundred #{teens_result}"
-      elsif (@array[0] == '0')
-        hundreds_result = hundreds() 
-        tens_result = tens()
-        return "#{hundreds_result} hundred #{tens_result}"
-      else
-        hundreds_result = hundreds() 
-        tens_result = tens()
-        ones_result = ones()
-        return "#{hundreds_result} hundred #{tens_result}-#{ones_result}"
-      end
-
+    if (@length == 4)
+      run_thousands()
+    elsif (@length == 3)
+      run_hundreds()
     elsif (@length == 2) 
-      if (@array[1] == '1') 
-        teens_result = get_teens()
-        return "#{teens_result}"
-      elsif(@array[0] == '0')
-        tens_result = tens()
-        return "#{tens_result}"
-      else
-        tens_result = tens()
-        ones_result = ones() 
-        return "#{tens_result}-#{ones_result}"
-      end
-
+      run_tens()
     else
-      ones_result = ones() 
-      return "#{ones_result}"
+      run_ones()
     end
-    
   end
-    
+
+  def run_thousands
+    if (@array[1] =='1' && @array[2] !='0')
+      return "#{thousands()} thousand #{run_hundreds()}" 
+    elsif  (@array[1] == '1')
+      return "#{thousands()} thousand #{teens()}"
+    elsif  (@array[0] == '0' && @array[1] == '0' && @array[2] != '0') 
+      return "#{thousands()} thousand #{run_hundreds()}"
+    elsif  (@array[0] != '0' && @array[1] != '0' && @array[2] == '0') 
+      return "#{thousands()} thousand #{run_tens()}"
+    elsif  (@array[0] == '0' && @array[1] != '0' && @array[2] == '0') 
+      return "#{thousands()} thousand #{run_tens()}"
+    elsif (@array[0] != '0' && @array[1] == '0' && @array[2] == '0')
+      return "#{thousands()} thousand and #{ones()}"
+    elsif (@array[0] == '0' && @array[1] == '0' && @array[2] == '0')
+      return "#{thousands()} thousand"
+    elsif (@array[0] == '0')
+      return "#{thousands()} thousand #{run_hundreds()}"
+    else
+      return "#{thousands()} thousand #{run_hundreds()}"
+    end
+  end
+
+  def run_hundreds
+    if (@array[1] == '1')
+      return "#{hundreds()} hundred #{teens()}"
+    elsif (@array[1] == '0' && @array[0] !='0')
+      return "#{hundreds()} hundred and #{ones()}"
+    elsif (@array[0] == '0' && @array[1] == '0')
+      return "#{hundreds()} hundred"
+    elsif (@array[0] == '0')
+      return "#{hundreds()} hundred #{tens()}"
+    else
+      return "#{hundreds()} hundred #{tens()}-#{ones()}"
+    end
+  end
+
+  def run_tens
+    if (@array[1] == '1') 
+      return "#{teens()}"
+    elsif(@array[0] == '0')
+      return "#{tens()}"
+    else
+      return "#{tens()}-#{ones()}"
+    end
+  end
+
+  def run_ones
+    return "#{ones()}"
+  end
+  
+  def thousands
+    @digits.fetch(@array[3])
+  end
 
   def hundreds
     @digits.fetch(@array[2]) 
@@ -57,7 +85,7 @@ class Number
     @digits.fetch(@array[0])  
   end
 
-  def get_teens()
+  def teens()
     if (@array[0] == "0")
       return "ten"
     elsif (@array[0] == "1")
@@ -80,29 +108,4 @@ class Number
       return "nineteen"
     end
   end
-
-  #   answer = []
-  #   number_array = @number.to_s().split('')
-  #   if (@number == 10)
-  #     answer.push('ten') 
-  #   elsif (@number == 11)
-  #       answer.push('eleven') 
-  #   elsif (@number == 12)
-  #     answer.push('twelve') 
-  #   elsif (@number == 13)
-  #     answer.push('thirteen') 
-  #   elsif (@number == 14)
-  #     answer.push('fourteen') 
-  #   elsif (@number == 15)
-  #     answer.push('fifteen') 
-  #   else (@number > 15 && @number < 20)
-  #     number_array.each do |number|
-  #       answer.push(@digits.fetch(number))
-  #     end
-  #     answer.shift()
-  #     answer.push('teen')
-  #     answer.join('')
-  #   end
-  #   return answer
-  # end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,20 +3,20 @@ require ('numbers_to_words')
 
 
 describe('Number#get_word') do
-  # it('takes a number and turns it into a string word') do
-  #   number = Number.new(1)
-  #   expect(number.numbers_to_words).to(eq("one"))
-  # end
+  it('takes a number and turns it into a string word') do
+    number = Number.new(1)
+    expect(number.get_word).to(eq("one"))
+  end
 
-  # it('turns numbers 10-19 into words') do
-  #   number = Number.new(17)
-  #   expect(number.numbers_to_words).to(eq("seventeen"))
-  # end
+  it('turns numbers 10-19 into words') do
+    number = Number.new(17)
+    expect(number.get_word).to(eq("seventeen"))
+  end
 
-  # it('turns numbers 20-29 into words') do
-  #   number = Number.new(21)
-  #   expect(number.numbers_to_words).to(eq("twenty-one"))
-  # end
+  it('turns numbers 20-29 into words') do
+    number = Number.new(21)
+    expect(number.get_word).to(eq("twenty-one"))
+  end
 
   it('turns 384 to three hundred eighty four') do
     number = Number.new(384)
@@ -42,4 +42,65 @@ describe('Number#get_word') do
     number = Number.new(0)
     expect(number.get_word).to(eq("zero"))
   end
+
+  it('turns 1342 into "one thousand three hundred fourty two"') do
+    number = Number.new(1342)
+    expect(number.get_word).to(eq("one thousand three hundred fourty-two"))
+  end
+
+  it('turns 100 into "one hundred"') do
+    number = Number.new(100)
+    expect(number.get_word).to(eq("one hundred"))
+  end
+
+  it('turns 205 into "one hundred"') do
+    number = Number.new(205)
+    expect(number.get_word).to(eq("two hundred and five"))
+  end
+
+  it('1000 to one thousand') do
+    number = Number.new(1000)
+    expect(number.get_word).to(eq("one thousand"))
+  end
+
+  it('1005 to "one thousand and five"') do
+    number = Number.new(1005)
+    expect(number.get_word).to(eq("one thousand and five"))
+  end
+
+  it('1050 to "one thousand fifty"') do
+    number = Number.new(1050)
+    expect(number.get_word).to(eq("one thousand fifty"))
+  end
+
+  it('1050 to "one thousand fifty"') do
+    number = Number.new(1050)
+    expect(number.get_word).to(eq("one thousand fifty"))
+  end
+
+  it('1050 to "one thousand fifty"') do
+    number = Number.new(1051)
+    expect(number.get_word).to(eq("one thousand fifty-one"))
+  end
+
+  it('3300 to "three thousand three hundred"') do
+    number = Number.new(3300)
+    expect(number.get_word).to(eq("three thousand three hundred"))
+  end
+
+  it('2015 to "two thousand fifteen"') do
+    number = Number.new(2015)
+    expect(number.get_word).to(eq("two thousand fifteen"))
+  end
+
+  it('2115 to "two thousand one hundred fifteen"') do
+    number = Number.new(2115)
+    expect(number.get_word).to(eq("two thousand one hundred fifteen"))
+  end
+
+  it('9350 to "nine thousand three hundred fifty"') do
+    number = Number.new(9350)
+    expect(number.get_word).to(eq("nine thousand three hundred fifty"))
+  end
+
 end
