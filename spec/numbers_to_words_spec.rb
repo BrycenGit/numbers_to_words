677a8ccca5358549b098f3133da3a1f791b71ced
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-22 11:38:39 -0700

    Write test for spec 5: turn numbers 20 - 29 into words
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -7,8 +7,13 @@ describe('Number#numbers_to_words') do
     expect(number.numbers_to_words).to(eq("one"))
   end
 
-  it('takes a number and turns it into a string word') do
+  it('turns numbers 10-19 into words') do
     number = Number.new(17)
     expect(number.numbers_to_words).to(eq("seventeen"))
+  end
+
+  it('turns numbers 20-29 into words') do
+    number = Number.new(21)
+    expect(number.numbers_to_words).to(eq("twenty-one"))
   end
 end
