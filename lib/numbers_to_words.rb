3dc0d7719a8d87ee3e41bfb8c215c14c6e33992d
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-22 11:15:41 -0700

    Pass test for spec 1 and 2: Return a written word for a single digit
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,4 +1,17 @@
-# class Number
-#   def initialize(number)
-#     @number = number
-#     @
+class Number
+  def initialize(number)
+    @number = number
+    @digits = {'0'=>"zero",'1'=>'one','2'=>'two','3'=>'three','4'=>'four','5'=>'five','6'=>'six','7'=>'seven','8'=>'eight','9'=>'nine'}
+  end
+
+  def numbers_to_words()
+    answer = []
+    number_array = @number.to_s().split('')
+    number_array.each do |number|
+      answer.push(@digits.fetch(number))
+    end
+    return answer.join(' ')
+  end
+
+
+end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,8 +2,8 @@ require ('rspec')
 require ('numbers_to_words')
 
 describe('Number#numbers_to_words') do
-  it('takes a number and turns it into a string') do
+  it('takes a number and turns it into a string word') do
     number = Number.new(1)
-    expect(number.numbers_to_words).to(eq("1"))
+    expect(number.numbers_to_words).to(eq("one"))
   end
 end
