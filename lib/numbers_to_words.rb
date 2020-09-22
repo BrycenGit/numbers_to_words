28250ccd7c2fe57a0b8c5854ac80c827c8024c59
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-22 11:35:19 -0700

    Pass test for spec 4
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -6,11 +6,32 @@ class Number
 
   def numbers_to_words()
     answer = []
+
     number_array = @number.to_s().split('')
-    number_array.each do |number|
-      answer.push(@digits.fetch(number))
+    if (@number == 10)
+      answer.push('ten') 
+    elsif (@number == 11)
+        answer.push('eleven') 
+    elsif (@number == 12)
+      answer.push('twelve') 
+    elsif (@number == 13)
+      answer.push('thirteen') 
+    elsif (@number == 14)
+      answer.push('fourteen') 
+    elsif (@number == 15)
+      answer.push('fifteen') 
+    elsif (@number > 15 && @number < 20)
+      number_array.each do |number|
+        answer.push(@digits.fetch(number))
+      end
+      answer.shift()
+      answer.push('teen')
+    else 
+      number_array.each do |number|
+        answer.push(@digits.fetch(number))
+      end
     end
-    return answer.join(' ')
+    return answer.join('')
   end
 
 
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -6,4 +6,9 @@ describe('Number#numbers_to_words') do
     number = Number.new(1)
     expect(number.numbers_to_words).to(eq("one"))
   end
+
+  it('takes a number and turns it into a string word') do
+    number = Number.new(17)
+    expect(number.numbers_to_words).to(eq("seventeen"))
+  end
 end
