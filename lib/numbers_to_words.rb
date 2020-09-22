d05cf404d5eeba4fb491b6b1b4c9e326453c1f35
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-22 14:19:58 -0700

    Pass all tests
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,38 +1,108 @@
 class Number
   def initialize(number)
     @number = number
+    @length = @number.to_s.length
+    @array = @number.to_s.split("").reverse()    
     @digits = {'0'=>"zero",'1'=>'one','2'=>'two','3'=>'three','4'=>'four','5'=>'five','6'=>'six','7'=>'seven','8'=>'eight','9'=>'nine'}
+    @tens = {'2'=>'twenty', '3'=>'thirty', '4'=>'fourty', '5'=>'fifty', '6'=>'sixty', '7'=>'seventy', '8'=>'eighty', '9'=>'ninety',}
   end
 
-  def numbers_to_words()
-    answer = []
+  def get_word 
+    if (@length == 3)
+      if (@array[1] == '1') 
+        hundreds_result = hundreds() 
+        teens_result = get_teens()
+        return "#{hundreds_result} hundred #{teens_result}"
+      elsif (@array[0] == '0')
+        hundreds_result = hundreds() 
+        tens_result = tens()
+        return "#{hundreds_result} hundred #{tens_result}"
+      else
+        hundreds_result = hundreds() 
+        tens_result = tens()
+        ones_result = ones()
+        return "#{hundreds_result} hundred #{tens_result}-#{ones_result}"
+      end
 
-    number_array = @number.to_s().split('')
-    if (@number == 10)
-      answer.push('ten') 
-    elsif (@number == 11)
-        answer.push('eleven') 
-    elsif (@number == 12)
-      answer.push('twelve') 
-    elsif (@number == 13)
-      answer.push('thirteen') 
-    elsif (@number == 14)
-      answer.push('fourteen') 
-    elsif (@number == 15)
-      answer.push('fifteen') 
-    elsif (@number > 15 && @number < 20)
-      number_array.each do |number|
-        answer.push(@digits.fetch(number))
+    elsif (@length == 2) 
+      if (@array[1] == '1') 
+        teens_result = get_teens()
+        return "#{teens_result}"
+      elsif(@array[0] == '0')
+        tens_result = tens()
+        return "#{tens_result}"
+      else
+        tens_result = tens()
+        ones_result = ones() 
+        return "#{tens_result}-#{ones_result}"
       end
-      answer.shift()
-      answer.push('teen')
-    else 
-      number_array.each do |number|
-        answer.push(@digits.fetch(number))
-      end
+
+    else
+      ones_result = ones() 
+      return "#{ones_result}"
     end
-    return answer.join('')
+    
+  end
+    
+
+  def hundreds
+    @digits.fetch(@array[2]) 
   end
 
+  def tens
+    @tens.fetch(@array[1]) 
+  end
 
+  def ones
+    @digits.fetch(@array[0])  
+  end
+
+  def get_teens()
+    if (@array[0] == "0")
+      return "ten"
+    elsif (@array[0] == "1")
+      return "eleven"
+    elsif (@array[0] == "2")
+      return "twelve"
+    elsif (@array[0] == "3")
+      return "thirteen"
+    elsif (@array[0] == "4")
+      return "fourteen"
+    elsif (@array[0] == "5")
+      return "fifteen"
+    elsif (@array[0] == "6")
+      return "sixteen"
+    elsif (@array[0] == "7")
+      return "seventeen"
+    elsif (@array[0] == "8")
+      return "eightteen"
+    else
+      return "nineteen"
+    end
+  end
+
+  #   answer = []
+  #   number_array = @number.to_s().split('')
+  #   if (@number == 10)
+  #     answer.push('ten') 
+  #   elsif (@number == 11)
+  #       answer.push('eleven') 
+  #   elsif (@number == 12)
+  #     answer.push('twelve') 
+  #   elsif (@number == 13)
+  #     answer.push('thirteen') 
+  #   elsif (@number == 14)
+  #     answer.push('fourteen') 
+  #   elsif (@number == 15)
+  #     answer.push('fifteen') 
+  #   else (@number > 15 && @number < 20)
+  #     number_array.each do |number|
+  #       answer.push(@digits.fetch(number))
+  #     end
+  #     answer.shift()
+  #     answer.push('teen')
+  #     answer.join('')
+  #   end
+  #   return answer
+  # end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,19 +1,45 @@
 require ('rspec')
 require ('numbers_to_words')
 
-describe('Number#numbers_to_words') do
-  it('takes a number and turns it into a string word') do
-    number = Number.new(1)
-    expect(number.numbers_to_words).to(eq("one"))
+
+describe('Number#get_word') do
+  # it('takes a number and turns it into a string word') do
+  #   number = Number.new(1)
+  #   expect(number.numbers_to_words).to(eq("one"))
+  # end
+
+  # it('turns numbers 10-19 into words') do
+  #   number = Number.new(17)
+  #   expect(number.numbers_to_words).to(eq("seventeen"))
+  # end
+
+  # it('turns numbers 20-29 into words') do
+  #   number = Number.new(21)
+  #   expect(number.numbers_to_words).to(eq("twenty-one"))
+  # end
+
+  it('turns 384 to three hundred eighty four') do
+    number = Number.new(384)
+    expect(number.get_word).to(eq("three hundred eighty-four"))
   end
 
-  it('turns numbers 10-19 into words') do
-    number = Number.new(17)
-    expect(number.numbers_to_words).to(eq("seventeen"))
+  it('turns teens into numbers') do
+    number = Number.new(314)
+    expect(number.get_word).to(eq("three hundred fourteen"))
   end
 
-  it('turns numbers 20-29 into words') do
-    number = Number.new(21)
-    expect(number.numbers_to_words).to(eq("twenty-one"))
+  it('turns two digit numbers with zero at the end into words') do
+    number = Number.new(30)
+    expect(number.get_word).to(eq("thirty"))
+  end
+
+  it('turns 3 digit numbers with zero at the end into words') do
+    number = Number.new(330)
+    expect(number.get_word).to(eq("three hundred thirty"))
+  end
+
+  it('returns zero for 0') do
+    number = Number.new(0)
+    expect(number.get_word).to(eq("zero"))
   end
 end
